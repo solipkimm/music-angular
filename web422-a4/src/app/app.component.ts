d38c6e70f205b47fad8689f4c6dcd1b8ef822201
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-03-12 05:14:31 -0500

    profile

--- web422-a4/src/app/app.component.ts
+++ web422-a4/src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
+* 
+*  Name: Solip Kim Student ID: 120618194 Date: March 12 2021
+*
+********************************************************************************/ 
+
 import { Component } from '@angular/core';
 
 @Component({
