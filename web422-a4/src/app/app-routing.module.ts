3825f62a086b6d72a52e39b54564a11bcfc17113
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-03-26 02:55:29 -0400

    path artist with id

--- web422-a4/src/app/app-routing.module.ts
+++ web422-a4/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: "newReleases", component: NewReleasesComponent},
-  { path: "artist", component: ArtistDiscographyComponent },
+  { path: "artist/:id", component: ArtistDiscographyComponent },
   { path: "album", component: AlbumComponent },
   { path: "about", component: AboutComponent },
   { path: "", redirectTo: '/newReleases', pathMatch: 'full' },
