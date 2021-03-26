7deeb88ec5c752a8735db50b19ef6aeec0a513a7
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-03-26 06:10:56 -0400

    update AppCompoenet

--- web422-a4/src/app/app.component.ts
+++ web422-a4/src/app/app.component.ts
@@ -9,12 +9,24 @@
 ********************************************************************************/ 
 
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web422-a4';
+  title = 'web422-a4a5';
+  searchString: string;
+
+  constructor(
+    private router: Router
+  ) {}
+
+  handleSearch() {
+    this.router.navigate(['/search'], {
+      queryParams: { q: this.searchString },
+    });
+    this.searchString = '';
+  }
 }
