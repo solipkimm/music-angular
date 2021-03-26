bf7a1c64728bf5cd20aa3f4e3de5d503e69ae54f
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-03-26 02:47:42 -0400

    update NewReleasesComponent

--- web422-a4/src/app/new-releases/new-releases.component.ts
+++ web422-a4/src/app/new-releases/new-releases.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../data/NewReleasesAlbums.json';
+import { MusicDataService } from '../music-data.service';
 
 @Component({
   selector: 'app-new-releases',
@@ -8,11 +9,15 @@ import * as data from '../data/NewReleasesAlbums.json';
 })
 export class NewReleasesComponent implements OnInit {
 
-  releases = data.albums.items;
+  releases = [];
+  private musicDataService: MusicDataService
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() { 
   }
 
+  ngOnInit(): void {
+    this.musicDataService.getNewReleases().subscribe((data)=>{
+      this.releases = data.albums.items;
+    })
+  }
 }
