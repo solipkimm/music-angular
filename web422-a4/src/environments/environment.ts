c9a41e332cc78e6dff9bf4b5be6def0aa3990723
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-04-09 01:12:29 -0400

    add userAPIBase

--- web422-a4/src/environments/environment.ts
+++ web422-a4/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   clientID: "cda2cb1f330a403483a78fefe225f412",
-  clientSecret: "62a5b480a41e4477890eb7a414278c3f"
+  clientSecret: "62a5b480a41e4477890eb7a414278c3f",
+  userAPIBase: "https://desolate-fortress-91840.herokuapp.com/api/users"
 };
 
 /*
