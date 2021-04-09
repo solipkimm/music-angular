8eb5c6d15992233fb80c943541f39ab6518b56f5
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-04-09 18:30:30 -0400

    update intercept-token

---
+++ web422-a4/src/app/intercept-token.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InterceptTokenService implements HttpInterceptor {
+
+  constructor(
+    private auth: AuthService
+  ) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!request.url.includes("spotify.com")) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `JWT ${this.auth.getToken()}`
+        }
+      });
+      return next.handle(request);
+    }
+    
+  }
+}
