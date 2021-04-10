0c0e6db2e732eced08958afc4afdfd24c32fabd4
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-04-09 23:03:32 -0400

    part 2

--- web422-a4/src/app/app.component.ts
+++ web422-a4/src/app/app.component.ts
@@ -1,15 +1,22 @@
 /*********************************************************************************
-*  WEB422 – Assignment 05
+*  WEB422 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
 *  assignment has been copied manually or electronically from any other source (including web sites) or 
 *  distributed to other students.
 * 
-*  Name: Solip Kim Student ID: 120618194 Date: March 26 2021
+*  Name: Solip Kim   Student ID: 120618194   Date: April 9, 2021
+*
+*  Online Link: _______________________________________________________________
 *
 ********************************************************************************/ 
 
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+import { AuthService } from './auth.service';
+import { User } from './User';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,9 +25,11 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'web422-a5';
   searchString: string;
+  token: User;
 
   constructor(
-    private router: Router
+    private router: Router, 
+    private auth: AuthService
   ) {}
 
   handleSearch() {
@@ -29,4 +38,18 @@ export class AppComponent {
     });
     this.searchString = '';
   }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.token = this.auth.readToken();
+      }
+    });
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+
 }
